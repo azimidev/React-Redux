f5ddf8380a16b8374b0983660b2de437d355c1d5
Hazz Azimi <info@azimi.dev>
2019-07-01 15:46:10 +0100

    Video 17

--- songs/src/components/SongDetail.js
+++ songs/src/components/SongDetail.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux'
 
-const SongDetail = props => {
-    console.log(props);
-    return <div>SongDetail</div>;
+const SongDetail = ({ song }) => {
+    if (!song) return <h5>Please select a song!</h5>;
+    return (
+        <div>
+            <h3>Details for:</h3>
+            <p>Title: { song.title }</p>
+            <p>Duration: { song.duration }</p>
+        </div>
+    );
 };
 
 const mapStateToProps = state => {
