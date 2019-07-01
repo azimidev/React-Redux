e221693ed86fdc2e2bbd2be9a8130896c026598b
Hazz Azimi <info@azimi.dev>
2019-07-01 18:16:34 +0100

    Video 22

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,10 +1,7 @@
 import jsonPlaceHolder from '../apis/jsonPlaceHolder';
 
-export const fetchPosts = async () => {
-    // BAD APPROACH! BAD DESIGN! AVOID!
+export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceHolder.get('/posts');
-    return {
-        type: 'FETCH_POSTS',
-        payload: response,
-    };
+
+    dispatch({ type: 'FETCH_POSTS', payload: response });
 };
