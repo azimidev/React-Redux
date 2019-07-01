920940c6e58b7b6520af43fc58dac646c763cc52
Hazz Azimi <info@azimi.dev>
2019-07-01 16:50:34 +0100

    Video 19

--- blog/src/components/App.js
+++ blog/src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import PostList from './PostList';
 
 const App = () => {
   return (
     <div className="ui container">
-      App
+      <PostList />
     </div>
   );
 };
---
+++ blog/src/components/PostList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class PostList extends Component {
+    render() {
+        return (
+            <div>
+                PostList
+            </div>
+        )
+    }
+};
+
