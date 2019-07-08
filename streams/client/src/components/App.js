4d8b7a306ed8c45d72b17a98bc6c5f6441b05546
Hazz Azimi <info@azimi.dev>
2019-07-08 16:22:12 +0100

    video 35

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
-const PageOne = () => <>Page One</>;
-const PageTwo = () => <>Page Two</>;
+const PageOne = () => <div>Home</div>;
+const PageTwo = () => <div>About</div>;
+const PageThree = () => <div>Contact</div>;
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
+
+        <p>
+          <Link to="/" >Home</Link>
+          &nbsp;&nbsp;&nbsp;
+          <Link to="/about" >About</Link>
+          &nbsp;&nbsp;&nbsp;
+          <Link to="/contact" >Contact</Link>
+        </p>
+
         <Route path="/" component={PageOne} exact />
         <Route path="/about" component={PageTwo} />
+        <Route path="/contact" component={PageThree} />
+
       </BrowserRouter>
     </div>
   );
