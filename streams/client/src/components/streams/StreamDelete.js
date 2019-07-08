f023c1ccca8ce00b092843bbd38bb188a7a16e55
Hazz Azimi <info@azimi.dev>
2019-07-08 17:05:39 +0100

    video 37

---
+++ streams/client/src/components/streams/StreamDelete.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function StreamDelete() {
+    return (
+        <div>
+            StreamDelete
+        </div>
+    );
+};
---
+++ streams/client/src/components/streams/StreamEdit.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function StreamEdit() {
+    return (
+        <div>
+            StreamEdit
+        </div>
+    );
+};
