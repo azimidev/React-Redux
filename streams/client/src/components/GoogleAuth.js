e378bd41e206764b31458bed134a7dbe5d92aebd
Hazz Azimi <info@azimi.dev>
2019-07-09 18:29:24 +0100

    video 41

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -4,6 +4,7 @@ export default class GoogleAuth extends Component {
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
+                // TODO: This is not going to work! Replace it with your Client ID
                 clientId: '1060645093787-u10abl9547l0dsvp3h2156dm1j930fnt.apps.googleusercontent.com',
                 scope: 'email',
             });
