542579080733548103ef586dd8dd406dbb537054
Hazz Azimi <info@azimi.dev>
2019-07-12 16:18:31 +0100

    video 49

--- streams/client/src/reducers/authReducer.js
+++ streams/client/src/reducers/authReducer.js
@@ -1,10 +1,12 @@
+import { SIGN_IN, SIGN_OUT } from '../actions/types';
+
 const INITIAL_STATE = { isSignedIn: null };
 
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        case 'SIGN_IN':
+        case SIGN_IN:
             return { ...state, isSignedIn: true };
-        case 'SIGN_OUT':
+        case SIGN_OUT:
             return { ...state, isSignedIn: false };
         default:
             return state;
