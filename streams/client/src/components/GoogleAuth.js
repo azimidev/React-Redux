136e994cd5ddb61d1bdbde43d52adf342b8dabac
Hazz Azimi <info@azimi.dev>
2019-07-12 14:43:06 +0100

    video 44

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -21,19 +21,27 @@ export default class GoogleAuth extends Component {
         this.setState({ isSignedIn: this.auth.isSignedIn.get() });
     }
 
+    onSignIn = () => {
+        this.auth.signIn();
+    }
+
+    onSignOut = () => {
+        this.auth.signOut();
+    }
+
     renderAuthButton() {
         if (this.state.isSignedIn === null) {
             return null;
         } else if (this.state.isSignedIn) {
             return (
-                <button className="ui red google button">
+                <button className="ui red google button" onClick={this.onSignOut}>
                     <i className="google icon" />
                     Sign Out
                 </button>
             );
         } else {
             return (
-                <button className="ui red google button">
+                <button className="ui red google button" onClick={this.onSignIn}>
                     <i className="google icon" />
                     Sign In
                 </button>
