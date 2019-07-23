f6e635997390c249217566cde96136633c318562
Hazz Azimi <info@azimi.dev>
2019-07-23 14:55:54 +0100

    video 55

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-export default function StreamCreate() {
-    return (
-        <div>
-            StreamCreate
-        </div>
-    );
+class StreamCreate extends React.Component {
+    renderInput({ input }) {
+        return <input {...input} />;
+    }
+
+    render() {
+        return (
+            <form>
+                <Field name="title" component={this.renderInput} />
+                <Field name="description" component={this.renderInput} />
+            </form>
+        );
+    };
 };
+
+export default reduxForm({ form: 'streamCreate' })(StreamCreate);
