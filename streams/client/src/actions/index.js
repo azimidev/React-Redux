ebd2dd765fd520a5fb2dbb25fe9925d05175ed98
Hazz Azimi <info@azimi.dev>
2019-07-23 17:57:58 +0100

    video 61

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -1,4 +1,5 @@
 import { SIGN_IN, SIGN_OUT } from './types';
+import streams from '../apis/streams';
 
 export const signIn = (userId) => {
     return {
@@ -11,4 +12,8 @@ export const signOut = () => {
     return {
         type: SIGN_OUT,
     };
+};
+
+export const createStream = formValues => async dispatch => {
+    streams.post('/streams', formValues);
 };
--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends React.Component {
 
