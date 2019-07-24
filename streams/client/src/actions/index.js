4b239a17c726011b13e8d36a0aefb4e73bcab13f
Hazz Azimi <info@azimi.dev>
2019-07-24 23:07:00 +0100

    video 73

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -8,6 +8,7 @@ import {
     EDIT_STREAM,
     DELETE_STREAM
 } from './types';
+import history from '../history';
 
 export const signIn = (userId) => {
     return { type: SIGN_IN, payload: userId };
@@ -21,8 +22,7 @@ export const createStream = (formValues) => async (dispatch, getState) => {
     const { userId } = getState().auth;
     const response = await streams.post('/streams', { ...formValues, userId });
     dispatch({ type: CREATE_STREAM, payload: response.data });
-
-    // TODO: Do some programatic navigation to get the user back to the root route
+    history.push('/');
 };
 
 export const fetchStreams = () => async dispatch => {
