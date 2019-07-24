6140c52a61aec1d0e87ad1bd82c6f15658e3eff4
Hazz Azimi <info@azimi.dev>
2019-07-24 14:57:30 +0100

    video 64

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -1,5 +1,13 @@
 import streams from '../apis/streams';
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from './types';
+import {
+    SIGN_IN,
+    SIGN_OUT,
+    FETCH_STREAMS,
+    CREATE_STREAM,
+    FETCH_STREAM,
+    EDIT_STREAM,
+    DELETE_STREAM
+} from './types';
 
 export const signIn = (userId) => {
     return { type: SIGN_IN, payload: userId };
@@ -9,8 +17,28 @@ export const signOut = () => {
     return { type: SIGN_OUT };
 };
 
-export const createStream = formValues => async dispatch => {
+export const createStream = (formValues) => async dispatch => {
     const response = await streams.post('/streams', formValues);
-
     dispatch({ type: CREATE_STREAM, payload: response.data });
+};
+
+export const fetchStreams = () => async dispatch => {
+    const response = await streams.get('/streams');
+    dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+export const fetchStream = (id) => async dispatch => {
+    const response = await streams.get(`/streams/${id}`);
+    dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+    const response = await streams.put(`/streams/${id}`, formValues);
+    dispatch({ type: EDIT_STREAM, payload: response.data });
+};
+
+export const deleteStream = (id) => async dispatch => {
+    // Nothing is returned so we don't need response
+    await streams.delete(`/streams/${id}`);
+    dispatch({ type: DELETE_STREAM, payload: id });
 };
