570f62e72b2066f08696c4a1bd1879246fd4cf2a
Hazz Azimi <info@azimi.dev>
2019-07-24 15:53:57 +0100

    video 66

---
+++ streams/client/src/reducers/streamReducer.js
@@ -0,0 +1,25 @@
+import {
+    FETCH_STREAMS,
+    FETCH_STREAM,
+    CREATE_STREAM,
+    EDIT_STREAM,
+    DELETE_STREAM
+} from '../actions/types';
+
+export default (state = {}, action) => {
+    switch (action.type) {
+        case FETCH_STREAM:
+            // const newState = {...state};
+            // newState[action.payload.id] = action.payload;
+            // return newState;
+            return { ...state, [action.payload.id]: action.payload };
+        case CREATE_STREAM:
+            return { ...state, [action.payload.id]: action.payload };
+        case EDIT_STREAM:
+            return { ...state, [action.payload.id]: action.payload };
+        case DELETE_STREAM:
+            // OMIT
+        default:
+            return state;
+    }
+};
