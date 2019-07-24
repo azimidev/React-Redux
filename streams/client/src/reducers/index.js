0d4f87b8f6fdb06a30f292e75fb5b2c5d19181bc
Hazz Azimi <info@azimi.dev>
2019-07-24 16:09:35 +0100

    video 67

--- streams/client/src/reducers/index.js
+++ streams/client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
+import streamReducer from './streamReducer';
 
 export default combineReducers({
     auth: authReducer,
     form: formReducer,
+    streams: streamReducer,
 });
 
--- streams/client/src/reducers/streamReducer.js
+++ streams/client/src/reducers/streamReducer.js
@@ -9,10 +9,9 @@ import {
 
 export default (state = {}, action) => {
     switch (action.type) {
+        case FETCH_STREAMS:
+            return { ...state, ..._.mapKeys(action.payload, 'id') };
         case FETCH_STREAM:
-            // const newState = {...state};
-            // newState[action.payload.id] = action.payload;
-            // return newState;
             return { ...state, [action.payload.id]: action.payload };
         case CREATE_STREAM:
             return { ...state, [action.payload.id]: action.payload };
