81c729f9e324e63656542e7e269417a77439ac16
Hazz Azimi <info@azimi.dev>
2019-07-24 22:58:27 +0100

    video 72

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -21,6 +21,8 @@ export const createStream = (formValues) => async (dispatch, getState) => {
     const { userId } = getState().auth;
     const response = await streams.post('/streams', { ...formValues, userId });
     dispatch({ type: CREATE_STREAM, payload: response.data });
+
+    // TODO: Do some programatic navigation to get the user back to the root route
 };
 
 export const fetchStreams = () => async dispatch => {
