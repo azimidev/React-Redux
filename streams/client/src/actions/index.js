75405159ecf33e71c7eaf51016c63dd408caa600
Hazz Azimi <info@azimi.dev>
2019-07-24 14:22:19 +0100

    video 63

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -1,19 +1,16 @@
-import { SIGN_IN, SIGN_OUT } from './types';
 import streams from '../apis/streams';
+import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from './types';
 
 export const signIn = (userId) => {
-    return {
-        type: SIGN_IN,
-        payload: userId,
-    };
+    return { type: SIGN_IN, payload: userId };
 };
 
 export const signOut = () => {
-    return {
-        type: SIGN_OUT,
-    };
+    return { type: SIGN_OUT };
 };
 
 export const createStream = formValues => async dispatch => {
-    streams.post('/streams', formValues);
+    const response = await streams.post('/streams', formValues);
+
+    dispatch({ type: CREATE_STREAM, payload: response.data });
 };
--- streams/client/src/actions/types.js
+++ streams/client/src/actions/types.js
@@ -1,2 +1,3 @@
 export const SIGN_IN = 'SIGN_IN';
 export const SIGN_OUT = 'SIGN_OUT';
+export const CREATE_STREAM = 'CREATE_STREAM';
