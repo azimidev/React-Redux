a58cb486b17f4c9a0bc6a896b53ae2d1d9c8f4d4
Hazz Azimi <info@azimi.dev>
2019-07-24 21:56:58 +0100

    video 69

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -17,8 +17,9 @@ export const signOut = () => {
     return { type: SIGN_OUT };
 };
 
-export const createStream = (formValues) => async dispatch => {
-    const response = await streams.post('/streams', formValues);
+export const createStream = (formValues) => async (dispatch, getState) => {
+    const { userId } = getState().auth;
+    const response = await streams.post('/streams', { ...formValues, userId });
     dispatch({ type: CREATE_STREAM, payload: response.data });
 };
 
