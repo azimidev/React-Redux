a458b72daed80b2db237e05ab6da53434493c4bf
Hazz Azimi <info@azimi.dev>
2019-07-24 16:00:19 +0100

    video 66

--- streams/client/src/reducers/streamReducer.js
+++ streams/client/src/reducers/streamReducer.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import {
     FETCH_STREAMS,
     FETCH_STREAM,
@@ -18,7 +19,7 @@ export default (state = {}, action) => {
         case EDIT_STREAM:
             return { ...state, [action.payload.id]: action.payload };
         case DELETE_STREAM:
-            // OMIT
+            return _.omit(state, action.id);
         default:
             return state;
     }
