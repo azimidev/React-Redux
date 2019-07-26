0947b63fde8a50ca26ad1dcce82d0b56d0a863e9
Hazz Azimi <info@azimi.dev>
2019-07-26 17:02:34 +0100

    video 79

--- streams/client/src/actions/types.js
+++ streams/client/src/actions/types.js
@@ -3,5 +3,5 @@ export const SIGN_OUT = 'SIGN_OUT';
 export const FETCH_STREAMS = 'FETCH_STREAMS'; // LIST (INDEX) -> (GET)
 export const CREATE_STREAM = 'CREATE_STREAM'; // CREATE (STORE) -> (POST)
 export const FETCH_STREAM = 'FETCH_STREAM'; // SHOW (READ) -> (GET)
-export const EDIT_STREAM = 'EDIT_STREAM'; // UPDATE (EDIT) -> (PUT)
+export const EDIT_STREAM = 'EDIT_STREAM'; // UPDATE (EDIT) -> (PATCH)
 export const DELETE_STREAM = 'DELETE_STREAM'; // DELETE (DESTROY) -> (DELETE)
