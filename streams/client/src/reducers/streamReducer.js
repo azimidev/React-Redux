8fd2a24c6d9304692cdffeba54ce495ceef6b5dd
Hazz Azimi <info@azimi.dev>
2019-07-28 22:28:04 +0100

    video 86

--- streams/client/src/reducers/streamReducer.js
+++ streams/client/src/reducers/streamReducer.js
@@ -18,7 +18,7 @@ export default (state = {}, action) => {
         case EDIT_STREAM:
             return { ...state, [action.payload.id]: action.payload };
         case DELETE_STREAM:
-            return _.omit(state, action.id);
+            return _.omit(state, action.payload);
         default:
             return state;
     }
