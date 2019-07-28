3b30d162cc4b23b6d5e465174c5308f81fc19cd4
Hazz Azimi <info@azimi.dev>
2019-07-28 20:34:35 +0100

    video 82

--- streams/client/src/components/Modal.js
+++ streams/client/src/components/Modal.js
@@ -6,14 +6,9 @@ export default (props) => {
     return ReactDOM.createPortal(
         <div className="ui dimmer modals visible active" onClick={() => history.push('/')}>
             <div className="ui standard modal visible active" onClick={(e) => e.stopPropagation()}>
-                <div className="header">Delete Stream</div>
-                <div className="content">
-                    Are you sure you want to delete this stream?
-                </div>
-                <div className="actions">
-                    <button className="ui red button">Delete</button>
-                    <button className="ui cancel button" onClick={(e) => history.push('/')}>Cancel</button>
-                </div>
+                <div className="header">{props.title}</div>
+                <div className="content">{props.content}</div>
+                <div className="actions">{props.actions}</div>
             </div>
         </div>,
         document.getElementById('modal')
--- streams/client/src/components/streams/StreamDelete.js
+++ streams/client/src/components/streams/StreamDelete.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import Modal from '../Modal';
+import history from '../../history';
 
 export default (props) => {
+    const actions = (
+        <React.Fragment>
+            <button className="ui negative button">Delete</button>
+            <button className="ui cancel button" onClick={(e) => history.push('/')}>Cancel</button>
+        </React.Fragment>
+    );
     return (
         <div>
-            <Modal />
+            <Modal
+                title="Delete Stream"
+                content="Are you sure you want to delete this stream?"
+                actions={actions}
+            />
         </div>
     );
 };
