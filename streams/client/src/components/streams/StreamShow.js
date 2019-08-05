2bbd9ff046e78d1ac6d7281064519a86e7bbafe4
Hazz Azimi <hassanazimi@users.noreply.github.com>
2019-08-05 18:03:15 +0100

    video 91

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -14,6 +14,26 @@ class StreamShow extends React.Component {
 
     componentDidMount() {
         this.props.fetchStream(this.props.match.params.id);
+        this.buildPlayer();
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        this.buildPlayer();
+    }
+
+
+    buildPlayer() {
+        if (this.player || !this.props.stream) {
+            return;
+        }
+
+        const { id } = this.props.match.params;
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        });
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load();
     }
 
     render() {
