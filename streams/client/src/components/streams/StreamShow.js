17a194a77b0dabc708e16a3796d6d8fadee72da1
Hazz Azimi <hassanazimi@users.noreply.github.com>
2019-08-05 17:32:34 +0100

    video 90

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import flv from 'flv.js';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
 
 
 class StreamShow extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.videoRef = React.createRef();
+    }
+
 
     componentDidMount() {
         this.props.fetchStream(this.props.match.params.id);
@@ -18,6 +25,11 @@ class StreamShow extends React.Component {
 
         return (
             <div>
+                <video
+                    ref={this.videoRef}
+                    style={{ width: '100%' }}
+                    controls
+                />
                 <h1>{title}</h1>
                 <h5>{description}</h5>
             </div>
