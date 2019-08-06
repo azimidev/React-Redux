f4b52ca6b8688d057cce3b41321d287f372b5d76
Hazz Azimi <hassanazimi@users.noreply.github.com>
2019-08-06 18:17:31 +0100

    video 97

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends Component {
+
+    static contextType = LanguageContext;
+
     render() {
+        console.log(this.context);
         return (
             <button className='button ui primary'>
                 Submit
@@ -9,5 +14,8 @@ class Button extends Component {
         );
     };
 }
+
+// This is another way of using context:
+// Button.contextType = LanguageContext;
 
 export default Button;
---
+++ translate/src/contexts/LanguageContext.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default React.createContext('english');
+
+// WHERE CAN I GET MY SOURCE OF DATA?
+// 1. Giving default value
+// 2. Provider from parent component
+
+// =================================
+
+// HOW TO USE CONTEXTS?
+// 1. this.context
+// 2. Consumer
